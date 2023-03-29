286bab7919b98902ab6779df30a011a8a16dc2af
PTMARKETING01 <ptmarketing01@MEGACENTRAL.local>
2023-03-29 16:22:50 +0200

    se ha añadido la funcion de obtener la ciudad del usuario

--- index.js
+++ index.js
@@ -31,26 +31,52 @@ function showPosition(position) {
     // console.log("Latitud: "+latitud.innerHTML);
     // console.log("Longitud: "+longitud.innerHTML);
     
+    getCity(position)
+
     crearUrl(latitud.innerHTML, longitud.innerHTML).then(lanzarPeticion);
     
 }
+
+// obtener la ciudad del usuario
+function getCity(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBt351dMXnP7LskzYpyeugrcX-OebFqkMM&latlng="+ lat + "," + lon +"&sensor=false";
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", url);
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                let respuesta = JSON.parse(httpRequest.responseText);
+                console.log(respuesta);
+                let ciudad = respuesta.results[0].address_components[2].long_name;
+                ciudad += ", " + respuesta.results[0].address_components[3].long_name;
+                let div = document.getElementById("ciudad");
+                div.innerHTML = ciudad;
+            }
+        }
+    }
+    httpRequest.send();
+}
+
+
 
 // muestra el error si no se puede obtener la posicion
 function showError(error) {
     let div = document.getElementById("datosTiempo");
     switch(error.code) {
       case error.PERMISSION_DENIED:
-        div.innerHTML = "<h2 class='text-danger fw-bolder'>User denied the request for Geolocation.</h2>"
+        div.innerHTML = "<h2 class='text-danger fw-bolder'>Has denegado el uso de la Geolocalización.</h2>"
         // console.log("User denied the request for Geolocation.")
         break;
       case error.POSITION_UNAVAILABLE:
-        div.innerHTML = "<h2 class='text-danger fw-bolder'>Location information is unavailable.</h2>"
+        div.innerHTML = "<h2 class='text-danger fw-bolder'>No se puede acceder a la localización.</h2>"
         break;
       case error.TIMEOUT:
-        div.innerHTML = "<h2 class='text-danger fw-bolder'>The request to get user location timed out.</h2>"
+        div.innerHTML = "<h2 class='text-danger fw-bolder'>Se ha excedido el tiempo necesario para obtener la localización del usuario.</h2>"
         break;
       case error.UNKNOWN_ERROR:
-        div.innerHTML = "<h2 class='text-danger fw-bolder'>An unknown error occurred.</h2>"
+        div.innerHTML = "<h2 class='text-danger fw-bolder'>A ocurrido un error inesperado.</h2>"
         break;
     }
 } 
@@ -79,7 +105,6 @@ function lanzarPeticion() {
         httpRequest.send();
 
     }
-    
 }
 
 // muestra los datos de la peticion
